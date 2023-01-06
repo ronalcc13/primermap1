a43dba11f9493432a99488d0aca2b67dbafdedeb
ronaldocc13 <ronaldocc13gmail.com>
2023-01-06 00:14:28 -0400

    con curvenavigation bar

---
+++ mapi2/lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+//const String google_api_key = "API _KEY";
+const Color prymaryColor = Colors.orangeAccent;
+const double defaultPadding = 20.0;
--- mapi2/lib/main.dart
+++ mapi2/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,31 +11,51 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-          ],
-        ),
-        body: Center(
-          child: SizedBox(
-            width: double.infinity,
-            //decoration: const BoxDecoration(color: Colors.blueAccent),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  'Ronaldo csc',
-                  style: TextStyle(
-                      color: Colors.orangeAccent,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30),
+          appBar: AppBar(
+            title: const Text('Material App Bar'),
+            actions: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+            ],
+          ),
+          body: Container(
+            decoration: BoxDecoration(color: Colors.red),
+            child: Center(
+              child: SizedBox(
+                width: double.infinity,
+                //decoration: const BoxDecoration(color: Colors.blueAccent),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Ronaldo cscas',
+                      style: TextStyle(
+                          color: Colors.orangeAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          bottomNavigationBar: CurvedNavigationBar(
+            backgroundColor: Colors.red,
+            index: 2,
+            items: const <Widget>[
+              Icon(Icons.home, size: 30),
+              Icon(Icons.search, size: 30),
+              Icon(Icons.add, size: 30),
+              Icon(Icons.favorite, size: 30),
+              Icon(Icons.person, size: 30),
+            ],
+            onTap: (index) {
+              //Handle button tap
+            },
+          )),
     );
   }
 }
