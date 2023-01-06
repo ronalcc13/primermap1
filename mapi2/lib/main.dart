aef907ea64cd12788cb2aac354d62ad35420e8d9
ronaldocc13 <ronaldocc13gmail.com>
2023-01-05 23:10:37 -0400

    Modificado parte de texto

--- mapi2/lib/main.dart
+++ mapi2/lib/main.dart
@@ -12,9 +12,27 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          ],
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: SizedBox(
+            width: double.infinity,
+            //decoration: const BoxDecoration(color: Colors.blueAccent),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Ronaldo csc',
+                  style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
